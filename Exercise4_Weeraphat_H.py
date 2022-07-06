efd77cb042f0bd09e7c44e37c1a3f3b81df56b83
nasrioppa <weeraphatp601@gmail.com>
2022-07-06 16:42:58 +0700

    Update Exercise4_Weeraphat_H.py

--- Exercise4_Weeraphat_H.py
+++ Exercise4_Weeraphat_H.py
@@ -2,7 +2,7 @@ FoundE = 88.8
 GenB = 79.2
 IntroCS = 77.5
 ComPr = 80
-
+print("--- Your Score ---")
 print("Foundation English :", FoundE, "score")
 print("General Business :", GenB, "score")
 print("Introduction to Computer Systems :", IntroCS, "score")
