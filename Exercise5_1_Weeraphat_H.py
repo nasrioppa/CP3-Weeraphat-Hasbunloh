804686762c0caaf13fceb1f89976d47339346bf8
nasrioppa <weeraphatp601@gmail.com>
2022-07-06 19:37:43 +0700

    coded

---
+++ Exercise5_1_Weeraphat_H.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Enter your number :"))
+num_2 = int(input("Enter your number :"))
+print(num_1, "+", num_2, "=" ,num_1 + num_2)
+print(num_1, "-", num_2, "=" ,num_1 - num_2)
+print(num_1, "*", num_2, "=" ,num_1 * num_2)
+print(num_1, "/", num_2, "=" ,num_1 / num_2)
