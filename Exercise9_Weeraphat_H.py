3ef2d1a944b3a4f1af7356caab84cfe95b1ec2e1
nasrioppa <weeraphatp601@gmail.com>
2022-07-07 18:20:32 +0700

    okokokok

---
+++ Exercise9_Weeraphat_H.py
@@ -0,0 +1,8 @@
+print("Please enter your Username and Password")
+usernameInput = input("Username :")
+passwordInput = input("password :")
+while usernameInput != "nasrioppa" or passwordInput != "58745" :
+    usernameInput = input("Username :")
+    passwordInput = input("password :")
+    print("Please try again !!")
+print("Done !")
