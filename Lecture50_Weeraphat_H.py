d92dfe65d228147944346cd6c98797c11e773c19
nasrioppa <weeraphatp601@gmail.com>
2022-07-08 01:01:46 +0700

    s

---
+++ Lecture50_Weeraphat_H.py
@@ -0,0 +1,13 @@
+def addNumber(x,y) :
+    print(x+y)
+def subNumber(x,y) :
+    print(x-y)
+def divNumber(x,y) :
+    print(x/y)
+def mulNumber(x,y) :
+    print(x*y)
+
+addNumber(25,5)
+subNumber(32,5)
+divNumber(10,52)
+mulNumber(25,66)
